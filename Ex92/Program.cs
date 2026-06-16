ceaf2ae5915f6f5427964c90a696f5dafd6e544c
ghzz27 <gabriel.comhenrique1@gmail.com>
2026-06-16 13:47:34 -0300

    commit

--- Ex92/Program.cs
+++ Ex92/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite N:");
+int n = int.Parse(Console.ReadLine());
 
+double A = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    A += (double)(n - i + 1) / i;
+}
+
+Console.WriteLine(A);
